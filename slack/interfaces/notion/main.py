53191017aae49c11dacda151a5dd95ab8ae35818
inwoo <inwooha00@naver.com>
2024-10-29 14:44:20 +0900

    Dev: check Database verification interface

--- slack/interfaces/notion/main.py
+++ slack/interfaces/notion/main.py
@@ -1,12 +1,25 @@
-import requests
-from ...schema import Diary, User
+import requests, logging
+from schema import Diary, User
 
+logger = logging.getLogger("notion")
+logger.addHandler(logging.FileHandler('./notion.log'))
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
 class Notion:
     url = "http://localhost:8010"
 
-    def setDiary(data:Diary, user: User):
-        requests.post(Notion.url + "/diary/set",{
+    def setDiary(data:Diary):
+        logger.info(f"data: {data.__dict__}")
+        requests.post(Notion.url + "/diary/set",{},{
             "title": data.title,
-            "content": data.context,
-            "files": data.files
+            "context": data.context,
+            "files": data.files,
+            "userId": data.user
         })
+
+    def checkDatabase(data:str):
+        logger.info(f"checking db id [{data}]")
+        res = requests.post(Notion.url+"/setting/verificate", {}, {
+            "dId": data
+        })
+        return res.json()
